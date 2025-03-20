621d33b15381ab7940ee02219c571764b18b23b7
Hyungoo Kang <hyungoo7703@gmail.com>
2025-03-20 17:10:46 +0900

    delete EntityScan

--- src/main/java/com/order/order_management_system/OrderManagementSystemApplication.java
+++ src/main/java/com/order/order_management_system/OrderManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.order.order_management_system.menu.infrastructure")
 public class OrderManagementSystemApplication {
 
 	public static void main(String[] args) {
